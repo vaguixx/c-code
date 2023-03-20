4bea5d615f5eb7f98c28e354b5a62b88eb0a3bc9
vaguixx <127215219+vaguixx@users.noreply.github.com>
2023-03-20 22:44:49 +0800

    Add files via upload

---
+++ 字符串逆序.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+//×Ö·û´®¼òµ¥ÄæÐò
+
+#include<stdio.h>
+#include<string.h>
+
+void reverse(char* src)
+{
+	int len = strlen(src);
+	char* left = src;
+	char* right = src + len-1;
+	char tmp = 0;
+	while (left <right)
+	{
+		
+		tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+	int i = 0;
+	for (i = 0; i < len; i++)
+	{
+		printf("%c  ", *(src + i));
+	}
+}
+
+int main()
+{
+	char arr[20] = "hello beijing.";
+	
+	reverse(arr);
+	//´òÓ¡
+	
+	return 0;
+}
---
+++ 题目1.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+//计算sn = a +aa +aaa + aaaa+ aaaaa
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	//首先要生成a,aa,aaa,aaaa,aaaaa
+//	int a = 0;
+//	int n = 0;
+//	int sum = 0;
+//	int ret = 0;
+//	scanf("%d%d", &a,&n);
+//	int i = 0;
+//	for (i = 0; i <n; i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum = sum + ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
